1943997c6a96e66ff30287c08adf3b2425c9c517
ghost <kirill.han011@gmail.com>
2022-04-25 15:48:21 +0600

    Создан класс Equipment. Описывает навесное оборудование и имеет свойства: уровень изношенности, занимаемый объём, название

--- models/equipment.py
+++ models/equipment.py
@@ -1,6 +1,6 @@
-# class Equipment:
-#     def __init__(self, name, taken_capacity) -> None:
-#         self.wear_condition = 0
-#         self.name = name
-#         self.taken_capacity = taken_capacity
-#
+class Equipment:
+    def __init__(self, name, taken_capacity) -> None:
+        self.wear_condition = 0
+        self.name = name
+        self.taken_capacity = taken_capacity
+
